5d6ed829ef440a4151d6a5763ad1eab30d7b739f
Noemi-Guarino <noemi.guarino.1996@gmail.com>
2024-02-06 13:31:46 +0100

    edit

--- index.php
+++ index.php
@@ -40,7 +40,7 @@
 
     ];
 
-    var_dump($hotels);
+    // var_dump($hotels);
 ?>
 
 <!DOCTYPE html>
@@ -57,29 +57,60 @@
             <table class="table">
             <thead>
                 <tr>
-                <th scope="col">#</th>
-                <th scope="col">First</th>
-                <th scope="col">Last</th>
-                <th scope="col">Handle</th>
+                <th scope="col">name</th>
+                <th scope="col">description</th>
+                <th scope="col">parking</th>
+                <th scope="col">vote</th>
+                <th scope="col">distance to center</th>
                 </tr>
             </thead>
             <tbody class="table-group-divider">
                 <tr>
-                <th scope="row">1</th>
-                <td>Mark</td>
-                <td>Otto</td>
-                <td>@mdo</td>
-                </tr>
-                <tr>
-                <th scope="row">2</th>
-                <td>Jacob</td>
-                <td>Thornton</td>
-                <td>@fat</td>
-                </tr>
-                <tr>
-                <th scope="row">3</th>
-                <td colspan="2">Larry the Bird</td>
-                <td>@twitter</td>
+                    <th scope="row">
+                        <ul>
+                            <?php
+                                foreach($hotels as $hotel) {
+                                    echo '<li>'.$hotel['name'].'</li>';
+                                }
+                            ?>
+                        </ul>
+                    </th>
+                    <td>
+                        <ul>
+                            <?php
+                                foreach($hotels as $hotel) {
+                                    echo '<li>'.$hotel['description'].'</li>';
+                                }
+                            ?>
+                        </ul>
+                    </td>
+                    <td>
+                        <ul>
+                            <?php
+                                foreach($hotels as $hotel) {
+                                    echo '<li>'.$hotel['parking'].'</li>';
+                                }
+                            ?>
+                        </ul>
+                    </td>
+                    <td>
+                        <ul>
+                            <?php
+                                foreach($hotels as $hotel) {
+                                    echo '<li>'.$hotel['vote'].'</li>';
+                                }
+                            ?>
+                        </ul>
+                    </td>
+                    <td>
+                        <ul>
+                            <?php
+                                foreach($hotels as $hotel) {
+                                    echo '<li>'.$hotel['distance_to_center'].'</li>';
+                                }
+                            ?>
+                        </ul>
+                    </td>
                 </tr>
             </tbody>
         </table>
